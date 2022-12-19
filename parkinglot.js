8b8c2101d3f4fdb7ebc01f8a6287a0e1f36ffbab
Pramudya Diva Alamsyah <pramudyaalamsyah@gmail.com>
2022-12-19 11:37:29 +0700

    ADD TIME VARIABLE

--- parkinglot.js
+++ parkinglot.js
@@ -1,13 +1,9 @@
 let data = {};
-// let parkingLotTime = 5000;
-// let parkTime = 3000;
-// let leaveTime = 1500;
-// let checkTime= 500;
 
-let parkingLotTime = 1
-let parkTime = 1
-let leaveTime = 1
-let checkTime= 1
+let parkingLotTime = 5000;
+let parkTime = 3000;
+let leaveTime = 1500;
+let checkTime= 500;
 
 class Car{
     constructor(nopol,pemilik){
@@ -122,14 +118,11 @@ async function wrap(){
         let car5 = new Car("B02E4","DIPA");
         await park(car5);
 
-
         let car6 = new Car("B02E5","DIPA");
         await park(car6);
 
-
         await leave("B02E6");
         await leave("B02E1");
-
       
         check();
     }catch(e){
